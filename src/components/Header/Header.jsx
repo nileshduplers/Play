ebc261cc1474e07da330fe9c24d74f9b0818b15d
nileshduplers <155451010+nileshduplers@users.noreply.github.com>
2024-02-01 16:05:43 +0530

    updated

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Header() {
+  return (
+    <header>
+      <div className="container mx-auto px-4">asd</div>
+    </header>
+  );
+}
+
+export default Header;
