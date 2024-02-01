856a80c2550efe156425941f63d928ef13380915
nileshduplers <155451010+nileshduplers@users.noreply.github.com>
2024-02-01 18:40:05 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from "react";
-import Header from "./components/Header/Header";
+import Header from "./components/Header";
+import Banner from "./components/Banner";
 
 function App() {
   return (
     <>
       <Header />
+      <Banner />
     </>
   );
 }
